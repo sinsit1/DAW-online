35ca548beaa7bfb64eba4231ac62e100f06fe58a
sinsit1 <alexandra8989@gmail.com>
2024-04-15 14:11:54 +0200

    Añade LENNGUAJE DE MARCAS ilerna

---
+++ ILERNA/LENGUAJES DE MARCAS/PAC DESARROLLO/PAC_UF1_alumno.zip/Ejercicio3/proceso.js
@@ -0,0 +1,57 @@
+
+const x = [
+    {
+    "Pais": "",
+    "Titulo": "Ori2inal",
+    "field3": "Castellano",
+    "Artista": "",
+    "Idioma": "",
+    "Video": ""
+    },
+    {
+    "Pais": "Alemania",
+    "Titulo": "Rockstars",
+    "field3": "Estrellas del rock",
+    "Artista": "Malik Harris",
+    "Idioma": "� Ingl�s",
+    "Video": "D3FN2umFaM8"
+    },
+    {
+    "Pais": "Espa�a",
+    "Titulo": "SloMo",
+    "field3": "C�mara lenta",
+    "Artista": "Chanel",
+    "Idioma": "� Espa�ol\n� Ingl�s",
+    "Video": "T02yGXVQCfo"
+    },
+    {
+    "Pais": "Finlandia",
+    "Titulo": "Jezebel",
+    "field3": "Jezebel",
+    "Artista": "The Rasmus",
+    "Idioma": "� Ingl�s",
+    "Video": "XtMfCf3vFbc"
+    },
+    {
+    "Pais": "Italia",
+    "Titulo": "Brividi",
+    "field3": "Escalofr�os",
+    "Artista": "Mahmood & Blanco",
+    "Idioma": "� Ita liano",
+    "Video": "4MgMhzfUmiA"
+    },
+    {
+    "Pais": "Ucrania",
+    "Titulo": "Stefania",
+    "field3": "Estefan�a",
+    "Artista": "Kalush Orchstra",
+    "Idioma": "� Ucraniano",
+    "Video": "UiEGVYOrulk"
+    }
+];
+
+const nuevo = []
+
+x.forEach(el =>{
+    console.log(el)
+})
